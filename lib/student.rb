82f7ce84313576ac46831624177933269d255e5c
Hoon Lee <yl.hoony@gmail.com>
2017-12-29 19:30:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,14 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(number)
-
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
